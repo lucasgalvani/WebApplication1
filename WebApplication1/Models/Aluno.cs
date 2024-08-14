30ccf4d24ba0cdb8c2fb214f45712c9602d28396
LUCAS GALVANI VIANNA AMARILLA <lucas.amarilla@alunos.sc.senac.br>
2024-08-13 21:31:39 -0300

    Deploy

---
+++ WebApplication1/Models/Aluno.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Aluno
+    {
+        public string Matricula { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set;}
+    }
+}
