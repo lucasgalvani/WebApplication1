bb5ca6aef54215d865c82a0fd10de561f8451ca7
LUCAS GALVANI VIANNA AMARILLA <lucas.amarilla@alunos.sc.senac.br>
2024-08-15 21:42:24 -0300

    Aula 15-08

---
+++ WebApplication1/Data/Mapeamento/AlunoMapeamento.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data.Mapeamento
+{
+    public class AlunoMapeamento : IEntityTypeConfiguration<Aluno>
+    {
+        public void Configure(EntityTypeBuilder<Aluno> builder)
+        {
+            builder.ToTable("Aluno");
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Matricula).HasColumnType("varchar(20)");
+            builder.Property(t => t.Nome).HasColumnType("varchar(50)");
+            builder.Property(t => t.Cpf).HasColumnType("varchar(14)");
+
+        }
+    }
+}
---
+++ WebApplication1/Data/Repositorio/AlunoRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using WebApplication1.Data.Repositorio.Interfaces;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data.Repositorio
+{
+    public class AlunoRepositorio : IAlunoRepositorio
+    {
+
+        private readonly BancoContexto _bancoContexto;
+
+        public AlunoRepositorio(BancoContexto bancoContexto)
+        {
+            _bancoContexto = bancoContexto;
+        }
+
+        public List<Aluno> BuscarAlunos()
+        {
+            return _bancoContexto.Aluno.ToList();
+        }
+    }
+}
